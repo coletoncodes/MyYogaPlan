a55308893a0d2055c6e9dfe52c284092514a6434
Coleton Gorecke <coletoncodes@gmail.com>
2024-06-14 21:52:03 -0500

    Initial Commit

---
+++ MyYogaPlan/MyYogaPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyYogaPlanApp.swift
+//  MyYogaPlan
+//
+//  Created by Coleton Gorecke on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct MyYogaPlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
