77bf17b27d64cbc03d7e5efaeb1f3a2d64b5dd45
Coleton Gorecke <coletoncodes@gmail.com>
2024-06-14 22:37:56 -0500

    Updated data models

--- MyYogaPlan/YogaCategories/YogaCategory.swift
+++ MyYogaPlan/YogaCategories/YogaCategory.swift
@@ -7,15 +7,39 @@
 
 import Foundation
 
-struct YogaCategory: Equatable, Identifiable, Codable {
-    var id: String
-    var name: String
-    var description: String
-    var poses: [YogaPose]
+struct YogaCategory: Identifiable, Codable, Equatable {
+    let id: Int
+    let categoryName, categoryDescription: String
+    let poses: [YogaPose]
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case categoryName = "category_name"
+        case categoryDescription = "category_description"
+        case poses
+    }
 }
 
-struct YogaPose: Equatable, Identifiable, Codable {
-    var id: String
-    var name: String
-    var difficulty: String
+// MARK: - Pose
+struct YogaPose: Identifiable, Codable, Equatable {
+    let id: Int
+    let categoryName, englishName, sanskritNameAdapted, sanskritName: String
+    let translationName, poseDescription, poseBenefits: String
+    let urlSVG: String
+    let urlPNG: String
+    let urlSVGAlt: String
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case categoryName = "category_name"
+        case englishName = "english_name"
+        case sanskritNameAdapted = "sanskrit_name_adapted"
+        case sanskritName = "sanskrit_name"
+        case translationName = "translation_name"
+        case poseDescription = "pose_description"
+        case poseBenefits = "pose_benefits"
+        case urlSVG = "url_svg"
+        case urlPNG = "url_png"
+        case urlSVGAlt = "url_svg_alt"
+    }
 }
